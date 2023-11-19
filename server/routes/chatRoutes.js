b0e62177ac71a4485f22e036b46ebaf83673ce82
devgarg <dev.garg@nagarro.com>
2023-11-19 17:34:36 +0530

    client

--- server/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { chat, getAllChats } from '../controller/chatController.js';
-
-const app = express.Router();
-
-app.get("/getAllChats/:id", getAllChats);
-
-export default app;
